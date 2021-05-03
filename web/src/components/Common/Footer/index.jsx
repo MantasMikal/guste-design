1f768698a95f974f3bf6a8504f86e7217b988563
Mantas Mikalauskas <mantas.mikalauskas@clock.co.uk>
2021-05-03 20:54:28 +0100

    feat: adds credits

--- web/src/components/Common/Footer/index.jsx
+++ web/src/components/Common/Footer/index.jsx
@@ -14,7 +14,17 @@ import ButtonStandard from 'Primitive/ButtonStandard'
 const Footer = ({ social, logo, siteTitle }) => {
   return (
     <Container as="footer" gutter className={styles.Footer}>
-        <Type size='base' className={styles.CopyRight}>© {new Date().getFullYear()} GUSTÉ.DESIGN <br /> <span>All rights reserved</span></Type>
+      <Type size="base" className={styles.CopyRight}>
+        © {new Date().getFullYear()} GUSTÉ.DESIGN. {' '}
+        <span>All rights reserved </span>
+        <br />
+        <span>
+          Crafted by{' '}
+          <SmartLink href="https://github.com/MantasMikal" target="__blank">
+            Mantas
+          </SmartLink>
+        </span>
+      </Type>
     </Container>
   )
 }
