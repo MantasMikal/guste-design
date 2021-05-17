2f80c4fed02f4d2a1cab217878bb01d8aebeb48a
MantasMikal <mantas.codes@gmail.com>
2021-05-17 13:40:21 +0100

    feat: mailer test

---
+++ web/functions/mail.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+
+const transporter = nodemailer.createTransport(
+  mg({
+    auth: {
+      api_key: process.env.MAILGUN_TOKEN,
+      domain: 'sandboxe796a32280c54800ab5158c6f4ce20f4.mailgun.org',
+    },
+  }),
+);
+
+exports.handler = async function (event) {
+  console.log(`Sending PDF report to ${email}`);
+  console.log(event.queryStringParameters)
+  const { email } = event.queryStringParameters
+
+  const info = await transporter.sendMail({
+    from: 'myemail@example.com',
+    to: email,
+    subject: 'Your report is ready!',
+    text: 'See attached report PDF',
+  });
+
+  console.log(`PDF report sent: ${info.messageId}`);
+};
