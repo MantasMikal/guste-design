645e308db3d8d9131c0a265058ecf340651cd9bd
MantasMikal <mantas.codes@gmail.com>
2022-07-15 23:49:20 +0300

    fix: typo

--- web/src/components/Section/Product/index.jsx
+++ web/src/components/Section/Product/index.jsx
@@ -27,7 +27,7 @@ const Product = ({ product, similarProducts, page }) => {
       <Stack>
         {/* <PageTitle title="Information" as="h3" /> */}
         <BlockContent blocks={_rawInformation} />
-        <PageTitle title="Similar products" as="h3" />
+        <PageTitle title="Similar art" as="h3" />
         <div className={styles.SimilarProductGrid}>
         {similarProducts.map((product) => (
           <ProductPreview
